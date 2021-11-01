2c90ca8bffd016a32ae25f2d10f6b08728de3e20
Michael Kenyi <michaelkenyi13@gmail.com>
2021-11-01 20:51:06 +0600

    gallery styling

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -9,94 +9,143 @@ function Gallery() {
           Our best times and memories in University of global village(UGV)
         </h1>
       </div>
-      <div className="max-w-5xl mx-auto">
+      <h2>Beginning of the Journey - 2019</h2>
+      <div className="mx-auto">
         <div className="grid grid-row-dense md:grid-col m-3 grid-cols-4">
-          <div className="">
+          <div>
             <img
-              className="w-60 h-80"
-              src={galleryData.hosam.image0}
-              alt="Hosam Nafez"
+              className="w-80 h-auto"
+              src={galleryData.ninetheen.image0}
+              alt="Palestine students-  UGV"
             />
             <div className="relative">
               <h5 className="z-10 absolute bottom-20  text-xl  text-white inline-flex">
-                {galleryData.hosam.title}
+                {galleryData.ninetheen.title0}
               </h5>
-              <p>{galleryData.hosam.desc0}</p>
+              <p>{galleryData.ninetheen.desc0}</p>
             </div>
           </div>
 
           <div>
             <img
-              className="w-60 h-80"
-              src={galleryData.michael.image0}
-              alt="michael Henry"
+              className="w-80 h-60"
+              src={galleryData.ninetheen.image1}
+              alt="sudanese students"
             />
-            <h5>{galleryData.michael.title}</h5>
-            <p>{galleryData.michael.desc0}</p>
+            <h5>{galleryData.ninetheen.title1}</h5>
+            <p>{galleryData.ninetheen.desc1}</p>
           </div>
 
           <div>
             <img
-              className="w-60 h-80"
-              src={galleryData.luka.image0}
-              alt="luka garang"
+              className="w-80 h-60"
+              src={galleryData.ninetheen.image2}
+              alt="internation students UGV"
             />
-            <h5>{galleryData.luka.title}</h5>
-            <p>{galleryData.luka.desc0}</p>
+            <h5>{galleryData.ninetheen.title2}</h5>
+            <p>{galleryData.ninetheen.desc2}</p>
           </div>
           <div>
             <img
-              className="w-60 h-80"
-              src={galleryData.emma.image0}
-              alt="emmanuel abbe"
+              className="w-80 h-60"
+              src={galleryData.ninetheen.image3}
+              alt="sudanese students"
             />
-            <h5>{galleryData.emma.title}</h5>
-            <p>{galleryData.emma.desc0}</p>
+            <h5>{galleryData.ninetheen.title3}</h5>
+            <p>{galleryData.ninetheen.desc3}</p>
           </div>
           <div>
             <img
-              className="w-60 h-80"
-              src={galleryData.abuka.image0}
-              alt="marilin abuka"
+              className="w-80 h-60"
+              src={galleryData.ninetheen.image4}
+              alt="Lawrence tigo"
             />
-            <h5>{galleryData.abuka.title}</h5>
-            <p>{galleryData.abuka.desc0}</p>
+            <h5>{galleryData.ninetheen.title4}</h5>
+            <p>{galleryData.ninetheen.desc4}</p>
           </div>
           <div>
             <img
-              className="w-60 h-80"
-              src={galleryData.Baha.image0}
-              alt="baha alhout"
+              className="w-80 h-60"
+              src={galleryData.ninetheen.image5}
+              alt="sudanese students"
             />
-            <h5>{galleryData.Baha.title}</h5>
-            <p>{galleryData.Baha.desc0}</p>
+            <h5>{galleryData.ninetheen.title5}</h5>
+            <p>{galleryData.ninetheen.desc5}</p>
           </div>
           <div>
             <img
-              className="w-60 h-80"
-              src={galleryData.duol.image0}
-              alt="duol jock"
+              className="w-80 h-60"
+              src={galleryData.ninetheen.image6}
+              alt="International students"
             />
-            <h5>{galleryData.duol.title}</h5>
-            <p>{galleryData.duol.desc0}</p>
+            <h5>{galleryData.ninetheen.title6}</h5>
+            <p>{galleryData.ninetheen.desc6}</p>
           </div>
           <div>
             <img
-              className="w-60 h-80"
-              src={galleryData.ben.image0}
-              alt="agolbeny malual"
+              className="w-80 h-60"
+              src={galleryData.ninetheen.image7}
+              alt="International students"
             />
-            <h5>{galleryData.ben.title}</h5>
-            <p>{galleryData.ben.desc0}</p>
+            <h5>{galleryData.ninetheen.title7}</h5>
+            <p>{galleryData.ninetheen.desc7}</p>
+          </div>
+        </div>
+        <h2>Our jouney start getting intense and trickier, but lovely- 2020</h2>
+        <div className="grid grid-row-dense md:grid-col m-3 grid-cols-4">
+          <div>
+            <img
+              className="w-80 h-60"
+              src={galleryData.twenty.image0}
+              alt="sudanese studanese"
+            />
+            <h5>{galleryData.twenty.title0}</h5>
+            <p>{galleryData.twenty.desc0}</p>
           </div>
           <div>
             <img
-              className="w-60 h-80"
-              src={galleryData.tigo.image0}
-              alt="lawrence tigo"
+              className="w-80 h-60"
+              src={galleryData.twenty.image1}
+              alt="University football club - UGV"
             />
-            <h5>{galleryData.tigo.title}</h5>
-            <p>{galleryData.tigo.desc0}</p>
+            <h5>{galleryData.twenty.title1}</h5>
+            <p>{galleryData.twenty.desc1}</p>
+          </div>
+          <div>
+            <img
+              className="w-80 h-60"
+              src={galleryData.twenty.image2}
+              alt="University football club - UGV"
+            />
+            <h5>{galleryData.twenty.title2}</h5>
+            <p>{galleryData.twenty.desc2}</p>
+          </div>
+          <div>
+            <img
+              className="w-80 h-60"
+              src={galleryData.twenty.image3}
+              alt="University football club - UGV"
+            />
+            <h5>{galleryData.twenty.title3}</h5>
+            <p>{galleryData.twenty.desc3}</p>
+          </div>
+          <div>
+            <img
+              className="w-80 h-60"
+              src={galleryData.twenty.image4}
+              alt="University football club - UGV"
+            />
+            <h5>{galleryData.twenty.title4}</h5>
+            <p>{galleryData.twenty.desc4}</p>
+          </div>
+          <div>
+            <img
+              className="w-80 h-60"
+              src={galleryData.twenty.image5}
+              alt="Sudanese students"
+            />
+            <h5>{galleryData.twenty.title5}</h5>
+            <p>{galleryData.twenty.desc5}</p>
           </div>
         </div>
       </div>
